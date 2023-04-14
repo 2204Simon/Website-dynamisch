a5d1cae8d93d1d0332c1f7d12f7418376762d1c7
2204Simon <sigl.simon@web.de>
2023-04-14 21:04:54 +0200

    Bugfix Click auf delivery-breakfast Logo

--- my-app/src/pages/Layout.js
+++ my-app/src/pages/Layout.js
@@ -1,54 +1,59 @@
 import { Outlet, Link } from "react-router-dom";
-import logo from '.././img/Logo.webp'
-import '../App.css';
+import logo from ".././img/Logo.webp";
+import "../App.css";
 const Layout = () => {
   return (
     <>
       <header id="zumSeitenanfang" className="header">
-          <p></p>
-          <div className="Logo">
-            <a href="index.html">
-              <img src={logo} alt="Logo" className="logo" width="100" height="100" />
-            </a>
+        <p></p>
+        <div className="Logo">
+          <a href="/">
+            <img
+              src={logo}
+              alt="Logo"
+              className="logo"
+              width="100"
+              height="100"
+            />
+          </a>
+        </div>
+        <nav>
+          <div className="navigation">
+            <input type="checkbox" className="toggle-menu" />
+            <div className="hamburger"></div>
+            <ul className="menu">
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/Produkte">Produkte</Link>
+              </li>
+              <li>
+                <Link to="/Bestellung">Bestellung</Link>
+              </li>
+              <li>
+                <Link to="/Unsere Geschichte">Unsere Geschichte</Link>
+              </li>
+              <li>
+                <Link to="/Kontakt">Kontakt</Link>
+              </li>
+            </ul>
           </div>
-          <nav>
-            <div className="navigation">
-              <input type="checkbox" className="toggle-menu" />
-              <div className="hamburger"></div>
-              <ul className="menu">
-                <li>
-                  <Link to="/">Home</Link>
-                </li>
-                <li>
-                  <Link to="/Produkte">Produkte</Link>
-                </li>
-                <li>
-                  <Link to="/Bestellung">Bestellung</Link>
-                </li>
-                <li>
-                  <Link to="/Unsere Geschichte">Unsere Geschichte</Link>
-                </li>
-                <li>
-                  <Link to="/Kontakt">Kontakt</Link>
-                </li>
-              </ul>
-            </div>
-          </nav>
-        </header>
+        </nav>
+      </header>
       <Outlet />
       <footer>
-            <div className="footer">
-              <a href="#zumSeitenanfang">zum Seitenanfang</a>
-              <a href="Impressum.html">Impressum</a>
-              <a href="Datenschutzerklärung.html">Datenschutzerklärung</a>
-              <div className="footer-content">
-                <p>copyright &copy;2022 delivery breakfast
-                </p>
-              </div>
-            </div>
-          </footer>
+        <div className="footer">
+          <a href="#zumSeitenanfang">zum Seitenanfang</a>
+          <a href="Impressum.html">Impressum</a>
+          <a href="Datenschutzerklärung.html">Datenschutzerklärung</a>
+          <div className="footer-content">
+            <p>copyright &copy;2022 delivery breakfast</p>
+          </div>
+        </div>
+      </footer>
     </>
-  )
+  );
 };
 
 export default Layout;
