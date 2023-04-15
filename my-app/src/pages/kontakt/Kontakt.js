5c6320b553c7c015bd552011a41d489574625dc1
2204Simon <sigl.simon@web.de>
2023-04-15 07:23:28 +0200

    responsive Karte

--- my-app/src/pages/kontakt/Kontakt.js
+++ my-app/src/pages/kontakt/Kontakt.js
@@ -7,6 +7,9 @@ import {
   FormTextArea,
   FormButton,
   FormLabel,
+  ContactList,
+  ContactListItem,
+  ContactMap,
 } from "./styles/Kontakt.styles";
 
 function Kontakt(): JSX.Element {
@@ -23,29 +26,21 @@ function Kontakt(): JSX.Element {
   return (
     <MainContainer>
       <h1>Kontakt</h1>
-      <p>Gerne kannst du uns auf folgenden Wegen erreichen:</p>
-      <ul>
-        <li>
+      <ContactList>
+        <ContactListItem>
           <Envelope size={24} />{" "}
           <a href="mailto:delivery-breakfast@outlook.de">
             delivery-breakfast@outlook.de
           </a>
-        </li>
-        <li>
+        </ContactListItem>
+        <ContactListItem>
           <Phone size={24} /> <a href="tel:+49123456789">0 123 456789</a>
-        </li>
-        <li>
+        </ContactListItem>
+        <ContactListItem>
           <MapPin size={24} /> Frühstücksstr. 20, 89518 Frühhausen
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2634.236118735561!2d10.152005076637723!3d48.68185011311377!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47991673b8ad167b%3A0x128ebc1169c962b2!2sDHBW%20Heidenheim!5e0!3m2!1sde!2sde!4v1681532862141!5m2!1sde!2sde"
-            width="600"
-            height="450"
-            allowfullscreen=""
-            loading="lazy"
-            referrerpolicy="no-referrer-when-downgrade"
-          ></iframe>
-        </li>
-      </ul>
+        </ContactListItem>
+      </ContactList>
+      <ContactMap />
       <hr />
       <ContactContainer>
         <h2>Newsletter abonnieren</h2>
--- my-app/src/pages/kontakt/styles/Kontakt.styles.js
+++ my-app/src/pages/kontakt/styles/Kontakt.styles.js
@@ -1,72 +1,136 @@
+import React from "react";
 import styled from "styled-components";
 
-// Styles for the main content container
 export const MainContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  margin: 0 auto;
-  padding: 2rem;
+  margin: 2rem auto;
+  max-width: 600px;
+  padding: 0 1rem;
+
+  h1 {
+    text-align: center;
+  }
+
+  ul {
+    list-style: none;
+    padding: 0;
+
+    li {
+      display: flex;
+      align-items: center;
+      margin-bottom: 1rem;
+
+      svg {
+        margin-right: 0.5rem;
+      }
+    }
+  }
+
+  hr {
+    margin: 2rem 0;
+    border: none;
+    border-top: 1px solid #ccc;
+  }
 `;
 
-// Styles for the contact form container
 export const ContactContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  max-width: 800px;
-`;
+  margin-top: 2rem;
 
-// Styles for the form input fields
-export const FormInput = styled.input`
-  width: 100%;
-  margin-bottom: 1rem;
-  padding: 0.5rem;
-  font-size: 1rem;
-  border: none;
-  border-radius: 5px;
-  box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.2);
-  &:focus {
-    outline: none;
+  h2 {
+    text-align: center;
+  }
+
+  p {
+    text-align: center;
+    margin-bottom: 1rem;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
   }
 `;
 
-// Styles for the form text area
-export const FormTextArea = styled.textarea`
-  width: 100%;
-  margin-bottom: 1rem;
-  padding: 0.5rem;
-  font-size: 1rem;
-  border: none;
-  border-radius: 5px;
-  box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.2);
-  &:focus {
-    outline: none;
-  }
-`;
-
-// Styles for the form submit button
-export const FormButton = styled.button`
-  padding: 0.5rem 1rem;
-  font-size: 1rem;
-  border: none;
-  border-radius: 5px;
-  background-color: #333;
-  color: #fff;
-  cursor: pointer;
-  transition: all 0.3s ease-in-out;
-  &:hover {
-    background-color: #fff;
-    color: #333;
-  }
-`;
-
-// Styles for the form input labels
 export const FormLabel = styled.label`
-  font-size: 1rem;
-  font-weight: bold;
-  margin-bottom: 0.5rem;
+  display: block;
+  margin-bottom: 1rem;
 `;
+
+export const FormInput = styled.input`
+  padding: 0.5rem;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  font-size: 1rem;
+  width: 100%;
+`;
+
+export const FormTextArea = styled.textarea`
+  padding: 0.5rem;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  font-size: 1rem;
+  width: 100%;
+  resize: vertical;
+`;
+
+export const FormButton = styled.button`
+  background-color: #2d9cdb;
+  color: #fff;
+  padding: 0.5rem 1rem;
+  border: none;
+  border-radius: 4px;
+  font-size: 1rem;
+  cursor: pointer;
+`;
+
+export const ContactList = styled.ul`
+  padding: 0;
+  margin: 0;
+`;
+
+export const ContactListItem = styled.li`
+  display: flex;
+  align-items: center;
+  margin-bottom: 1rem;
+
+  svg {
+    margin-right: 0.5rem;
+  }
+`;
+
+export const MapContainer = styled.div`
+  position: relative;
+  padding-bottom: 56.25%;
+  height: 0;
+  overflow: hidden;
+
+  iframe {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+function ContactMap(): JSX.Element {
+  return (
+    <MapContainer>
+      <iframe
+        title="Map"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2634.236118735561!2d10.152005076637723!3d48.68185011311377!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47991673b8ad167b%3A0x128ebc1169c962b2!2sDHBW%20Heidenheim!5e0!3m2!1sde!2sde!4v1681532862141!5m2!1sde!2sde"
+        width="600"
+        height="450"
+        style={{ border: 0 }}
+        allowFullScreen={false}
+        loading="lazy"
+      />
+    </MapContainer>
+  );
+}
+
+export const media = {
+  tablet: "@media(min-width: 768px)",
+  laptop: "@media(min-width: 1024px)",
+};
+export { ContactMap };
