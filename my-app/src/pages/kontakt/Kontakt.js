2b0a3be20f72a5ce83a66d72d861b926e888dedd
2204Simon <sigl.simon@web.de>
2023-04-15 06:38:13 +0200

    Google Maps Karte einbinden

--- my-app/src/pages/kontakt/Kontakt.js
+++ my-app/src/pages/kontakt/Kontakt.js
@@ -26,16 +26,24 @@ function Kontakt(): JSX.Element {
       <p>Gerne kannst du uns auf folgenden Wegen erreichen:</p>
       <ul>
         <li>
-          <MapPin size={24} /> Frühstücksstr. 20, 89518 Frühhausen
+          <Envelope size={24} />{" "}
+          <a href="mailto:delivery-breakfast@outlook.de">
+            delivery-breakfast@outlook.de
+          </a>
         </li>
         <li>
           <Phone size={24} /> <a href="tel:+49123456789">0 123 456789</a>
         </li>
         <li>
-          <Envelope size={24} />{" "}
-          <a href="mailto:delivery-breakfast@outlook.de">
-            delivery-breakfast@outlook.de
-          </a>
+          <MapPin size={24} /> Frühstücksstr. 20, 89518 Frühhausen
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2634.236118735561!2d10.152005076637723!3d48.68185011311377!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47991673b8ad167b%3A0x128ebc1169c962b2!2sDHBW%20Heidenheim!5e0!3m2!1sde!2sde!4v1681532862141!5m2!1sde!2sde"
+            width="600"
+            height="450"
+            allowfullscreen=""
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+          ></iframe>
         </li>
       </ul>
       <hr />
