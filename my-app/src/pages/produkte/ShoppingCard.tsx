3629a0c0accdb1cc6f1097fd785161dba6b499e6
ThorstenLicht <114730064+ThorstenLicht@users.noreply.github.com>
2023-04-16 13:01:29 +0200

    Cartarray

---
+++ my-app/src/pages/produkte/ShoppingCard.tsx
@@ -0,0 +1,83 @@
+import React, { useState, ChangeEvent } from "react";
+import {
+  Container,
+  Details,
+  ImageContainer,
+  Price,
+  Quantity,
+  QuantityInput,
+  Title,
+  //styles,
+} from "./styles/ShoppingCard.styles";
+import Logo from "../../img/Logo.webp";
+import { BlackButton } from "../general/button";
+import { log } from "console";
+
+interface ShoppingCardProps {
+  image: string;
+  title: string;
+  price: number;
+}
+
+interface TestArrayItem {
+  anzahl: number;
+  preis: number;
+  logo: string;
+  produktname: string;
+}
+
+type TestArray = TestArrayItem[];
+export const CartArray: TestArray = [];
+
+const ShoppingCard: React.FC<ShoppingCardProps> = ({ image, title, price }) => {
+  const [quantity, setQuantity] = useState<number>(0);
+
+  const handleQuantityChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setQuantity(Number(event.target.value));
+  };
+
+  function AddToCart(
+    productname: string,
+    logo: string,
+    price: number,
+    anzahl: number
+  ): void {
+    console.log("string");
+    CartArray.push({
+      anzahl: anzahl,
+      preis: price,
+      logo: logo,
+      produktname: productname,
+    });
+    console.log(CartArray);
+    // add logic to add item to cart
+  }
+
+  return (
+    <Container>
+      <ImageContainer>
+        {/* <Image src={Logo} alt="Product" /> */}
+      </ImageContainer>
+      <Details>
+        <h2>{title}</h2>
+        <Price>Price: {price} €</Price>
+        <Quantity>
+          <label htmlFor="quantity">Quantity:</label>
+          <QuantityInput
+            type="number"
+            id="quantity"
+            name="quantity"
+            min="0"
+            value={quantity}
+            onChange={handleQuantityChange}
+          />
+        </Quantity>
+        <button onClick={() => AddToCart(title, image, price, quantity)}>
+          Zum Warenkorb hinzufügen
+        </button>
+      </Details>
+    </Container>
+  );
+};
+
+export default ShoppingCard;
--- my-app/src/pages/produkte/styles/ShoppingCard.styles.js
+++ my-app/src/pages/produkte/styles/ShoppingCard.styles.js
@@ -1,54 +1,64 @@
-export const styles = {
-    container: {
-      display: 'flex',
-      flexDirection: 'column',
-      backgroundColor: '#fff',
-      borderRadius: '10px',
-      boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)',
-      padding: '1rem',
-      maxWidth: '250px',
-      margin: '1rem',
-      cursor: 'pointer',
-      transition: 'transform 0.3s ease-in-out',
-    },
-    imageContainer: {
-      overflow: 'hidden',
-      borderRadius: '10px',
-      marginBottom: '1rem',
-    },
-    image: {
-      width: '100%',
-      height: 'auto',
-      transition: 'transform 0.3s ease-in-out',
-    },
-    details: {
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-    },
-    title: {
-      margin: '0',
-      fontSize: '1.5rem',
-      textAlign: 'center',
-    },
-    price: {
-      margin: '0',
-      fontSize: '1.2rem',
-    },
-    quantity: {
-      display: 'flex',
-      alignItems: 'center',
-      margin: '1rem 0',
-    },
-    quantityInput: {
-      width: '3rem',
-      padding: '0.5rem',
-      border: '1px solid #aaa',
-      borderRadius: '5px',
-      marginLeft: '0.5rem',
-      fontSize: '1.2rem',
-      textAlign: 'center',
-      appearance: 'textfield',
-    },
-  
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: #fff;
+  border-radius: 10px;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
+  padding: 1rem;
+  max-width: 250px;
+  margin: 1rem;
+  cursor: pointer;
+  transition: transform 0.3s ease-in-out;
+
+  &:hover {
+    transform: scale(1.05);
   }
+`;
+
+export const ImageContainer = styled.div`
+  overflow: hidden;
+  border-radius: 10px;
+  margin-bottom: 1rem;
+`;
+
+export const Image = styled.img`
+  width: 100%;
+  height: auto;
+  transition: transform 0.3s ease-in-out;
+`;
+
+export const Details = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Title = styled.h2`
+  margin: 0;
+  font-size: 1.5rem;
+  text-align: center;
+`;
+
+export const Price = styled.p`
+  margin: 0;
+  font-size: 1.2rem;
+`;
+
+export const Quantity = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 1rem 0;
+`;
+
+export const QuantityInput = styled.input`
+  width: 3rem;
+  padding: 0.5rem;
+  border: 1px solid #aaa;
+  border-radius: 5px;
+  margin-left: 0.5rem;
+  font-size: 1.2rem;
+  text-align: center;
+  appearance: textfield;
+`;
