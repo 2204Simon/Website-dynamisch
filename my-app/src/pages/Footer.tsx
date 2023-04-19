0e43e1ba61411874823d325b673cec199a66fa03
2204Simon <sigl.simon@web.de>
2023-04-19 19:29:33 +0200

    Footer neue Seite aufmachen

--- my-app/src/pages/Footer.tsx
+++ my-app/src/pages/Footer.tsx
@@ -27,27 +27,50 @@ function Footer(): JSX.Element {
           </ul>
         </div>
         <div className="social-media">
-          <a href="https://twitter.com/deliverybreak">
+          <a
+            href="https://twitter.com/deliverybreak"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <TwitterLogo size={24} />
           </a>
-          <a href="https://www.facebook.com/profile.php?id=100091669221469&sk=about">
+          <a
+            href="https://www.facebook.com/profile.php?id=100091669221469&sk=about"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <FacebookLogo size={24} />
           </a>
-          <a href="https://www.instagram.com/delivery_breakfast_">
+          <a
+            href="https://www.instagram.com/delivery_breakfast_"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <InstagramLogo size={24} />
           </a>
-          <a href="https://www.tiktok.com/@delivery_breakfast?is_from_webapp=1&sender_device=pc">
+          <a
+            href="https://www.tiktok.com/@delivery_breakfast?is_from_webapp=1&sender_device=pc"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <TiktokLogo size={24} />
           </a>
-          <a href="https://t.snapchat.com/E0HWtcis">
+          <a
+            href="https://t.snapchat.com/E0HWtcis"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <SnapchatLogo size={24} />
           </a>
-          <a href="https://www.linkedin.com/in/delivery-breakfast-723b46272">
+          <a
+            href="https://www.linkedin.com/in/delivery-breakfast-723b46272"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <LinkedinLogo size={24} />
           </a>
           <li>
             <Link to="/">
-              {" "}
               &copy; delivery-breakfast {new Date().getFullYear()}
             </Link>
           </li>
