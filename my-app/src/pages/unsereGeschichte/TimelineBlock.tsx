b0475064a07d8eda409cf244e67dd517105d8413
JoHerr02 <72371985+JoHerr02@users.noreply.github.com>
2023-04-21 09:46:02 +0200

    Verbes. Code + Einbau Bilder auf Unsere_Gechichte

--- my-app/src/pages/unsereGeschichte/TimelineBlock.tsx
+++ my-app/src/pages/unsereGeschichte/TimelineBlock.tsx
@@ -1,58 +1,46 @@
 import React, { useState, ChangeEvent } from "react";
 import {
-  Container,
-  Timeline_further_information,
-  ImageContainer,
-  Timeline_year,
-  Timeline_image,
-  Timeline_header_sentence,
-  Details,
+  TimelineItemA,
+  TimelineContent,
   Image,
 } from "./TimelineBlock.styles";
-import { BlackColorButton } from "../general/button";
-/*import { Details } from "../produkte/styles/ShoppingCard.styles";*/
 
 
-interface TimelineBlockProperties {
-  /*timeline_image: string;
-  timeline_year: string;
-  timeline_header_sentence: string;
-  timeline_further_information: string;*/
+interface TimelineItemProps {
+  date: string;
+  title: string;
+  description: string;
+  isFirst?: boolean;
+  details: string;
   image: string;
 }
 
-
-
-const TimelineBlock: React.FC<TimelineBlockProperties> = ({ /*timeline_image, timeline_year, timeline_header_sentence, timeline_further_information, image */}) => {
-  const [quantity, setQuantity] = useState<number>(0);
-
-  const handleQuantityChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setQuantity(Number(event.target.value));
-  };
-
- /* return (
-    <Container>
-      <ImageContainer>
-        <Image src={image} alt="TimelineBlock" />
-      </ImageContainer>
-      <Details>
-        <Timeline_year>{timeline_year}</Timeline_year>
-        <Timeline_header_sentence>{timeline_header_sentence}</Timeline_header_sentence>
-        <Timeline_further_information>{timeline_further_information}</Timeline_further_information>
-        <BlackColorButton
-          caption="Mehr Informationen"
-        />
-      </Details>
-    </Container>
-  );*/
+const TimelineItem: React.FC<TimelineItemProps> = ({
+  date,
+  title,
+  description,
+  isFirst = false,
+  details,
+  image,
+}) => {
+  const dotClass = isFirst ? "timeline-dot first" : "timeline-dot";
+  const dateClass = isFirst ? "timeline-date first" : "timeline-date";
 
   return (
-   <Container>
-      <ImageContainer>
-        {/* <Image src={image} alt="TimelineBlock" /> */}
-      </ImageContainer>
-      </Container>
-    );
+    <TimelineItemA>
+      <div className={dotClass} />
+      <div className={dateClass}>{date}</div>
+
+        <TimelineContent>
+        <Image src={image} alt="product" />
+        <h3 style={{ color: "black" }}>{title}</h3>
+        <p>{description}</p>
+        <details>{details}</details>
+        </TimelineContent>
+
+    </TimelineItemA>
+
+  );
 };
 
-export default TimelineBlock;
+export default TimelineItem;
