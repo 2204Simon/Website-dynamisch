ff9027495ad9e4d1887e6c515e470a1701653c88
ThorstenLicht <114730064+ThorstenLicht@users.noreply.github.com>
2023-04-22 16:27:53 +0200

    Rechts Links Pfeile bei Essen

--- my-app/src/pages/produkte/Scroll.tsx
+++ my-app/src/pages/produkte/Scroll.tsx
@@ -1,6 +1,7 @@
 import React, { useRef, useEffect, useState } from "react";
 import styled from "styled-components";
-import { ArrowRight, ArrowLeft } from "phosphor-react";
+import { ArrowCircleLeft, ArrowCircleRight } from "phosphor-react";
+import { colors } from "../general/constants";
 
 interface ScrollContainerProps {
   children: React.ReactNode;
@@ -43,46 +44,53 @@ const ScrollContainer: React.FC<ScrollContainerProps> = ({
 
   useEffect(() => {
     checkScroll();
-  }, []);
+  }, [scrollLeftVisible, scrollRightVisible]);
 
   const ArrowContainer = styled.div`
-    position: relative;
-    top: 300px;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    margin-top: 250px;
     padding: 10px;
     display: flex;
     justify-content: space-between;
-  `;
-
-  const ArrowLeft = styled.div<{ visible: boolean }>`
-    cursor: pointer;
-    z-index: 1;
-    background-color: #ccc;
-    padding: 5px;
-    border-radius: 50%;
-    visibility: ${({ visible }) => (visible ? "visible" : "hidden")};
-  `;
-
-  const ArrowRight = styled.div<{ visible: boolean }>`
-    cursor: pointer;
-    z-index: 1;
-    background-color: #ccc;
-    padding: 5px;
-    border-radius: 50%;
-    visibility: ${({ visible }) => (visible ? "visible" : "hidden")};
+    z-index: 0;
   `;
 
   return (
-    <div className={className}>
+    <div style={{ margin: 0, padding: 0 }}>
       <ArrowContainer>
-        <ArrowLeft visible={scrollLeftVisible} onClick={scrollLeft}>
-          {"<"}
-        </ArrowLeft>
-        <ArrowRight visible={scrollRightVisible} onClick={scrollRight}>
-          {">"}
-        </ArrowRight>
+        <ArrowCircleLeft
+          style={{
+            zIndex: "2",
+            backgroundColor: colors.companycolor,
+            borderRadius: "50px",
+            visibility: scrollLeftVisible ? "visible" : "hidden",
+          }}
+          size={50}
+          onClick={scrollLeft}
+          cursor={"pointer"}
+        />
+        <ArrowCircleRight
+          style={{
+            zIndex: "2",
+            backgroundColor: colors.companycolor,
+            borderRadius: "50px",
+            visibility: scrollRightVisible ? "visible" : "hidden",
+          }}
+          size={50}
+          onClick={scrollRight}
+          cursor={"pointer"}
+        />
       </ArrowContainer>
       <div
-        style={{ display: "flex", flexWrap: "nowrap", overflowX: "hidden" }}
+        style={{
+          display: "flex",
+          flexWrap: "nowrap",
+          overflowX: "hidden",
+          marginTop: 0,
+          paddingTop: 0,
+        }}
         ref={containerRef}
       >
         {children}
