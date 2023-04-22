b2bef667a4d54b79f75b7496750e6edb8543537d
2204Simon <sigl.simon@web.de>
2023-04-22 13:42:30 +0200

    automatisch seite wechseln und ontop scrolling

--- my-app/src/BottomNavBar.tsx
+++ my-app/src/BottomNavBar.tsx
@@ -15,27 +15,30 @@ function BottomNavBar(): JSX.Element {
         <div className="navigation bottom-nav">
           <ul className="menu">
             <li>
-              <Link to="/">
+              <Link to="/" onClick={() => window.scrollTo(0, 0)}>
                 <HouseLine size={40} weight="fill" />
               </Link>
             </li>
             <li>
-              <Link to="/Produkte">
+              <Link to="/Produkte" onClick={() => window.scrollTo(0, 0)}>
                 <ShoppingBagOpen size={40} weight="fill" />
               </Link>
             </li>
             <li>
-              <Link to="/Bestellung">
+              <Link to="/Bestellung" onClick={() => window.scrollTo(0, 0)}>
                 <ShoppingCart size={40} weight="fill" />
               </Link>
             </li>
             <li>
-              <Link to="/Unsere Geschichte">
+              <Link
+                to="/Unsere Geschichte"
+                onClick={() => window.scrollTo(0, 0)}
+              >
                 <ClockCounterClockwise size={40} weight="fill" />
               </Link>
             </li>
             <li>
-              <Link to="/Kontakt">
+              <Link to="/Kontakt" onClick={() => window.scrollTo(0, 0)}>
                 <AddressBook size={40} weight="fill" />
               </Link>
             </li>
