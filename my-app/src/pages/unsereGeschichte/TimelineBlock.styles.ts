475b9eb1b758dafd076171055085b92146fca861
JoHerr02 <72371985+JoHerr02@users.noreply.github.com>
2023-04-24 20:48:03 +0200

    Weiterbau unsere_Geschichte

--- my-app/src/pages/unsereGeschichte/TimelineBlock.styles.ts
+++ my-app/src/pages/unsereGeschichte/TimelineBlock.styles.ts
@@ -1,42 +1,5 @@
 import styled from "styled-components";
 
-export const Timeline_image = styled.img`
-  width: 100%;
-  height: auto;
-  transition: transform 0.3s ease-in-out;
-`;
-
-export const Timeline_year = styled.h2`
-  margin: 0;
-  font-size: 1.5rem;
-  text-align: center;
-  color: black;
-`;
-
-export const Timeline_header_sentence = styled.h3``;
-
-export const Timeline_further_information = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
-export const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  background-color: #ffffff;
-  border-radius: 10px;
-  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
-  padding: 1rem;
-  max-width: 250px;
-  margin: 1rem;
-  cursor: pointer;
-  transition: transform 0.3s ease-in-out;
-
-  &:hover {
-    transform: scale(1.05);
-  }
-`;
 
 export const ImageContainer = styled.div`
   overflow: hidden;
@@ -169,6 +132,7 @@ export const TimelineParagraph = styled.p`
   color: #000;
   margin: 16px 0px 0px 0px;
 `;
+
 
 
 /* 
