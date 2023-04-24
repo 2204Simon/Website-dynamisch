5464bb8b55ed10b86b106647728f8b777c8a492e
2204Simon <sigl.simon@web.de>
2023-04-24 10:17:52 +0200

    Footer anpassung

---
+++ my-app/src/pages/Footer.styles.ts
@@ -0,0 +1,56 @@
+import styled from "styled-components";
+import { colors } from "./general/constants";
+
+export const FooterContainer = styled.footer`
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: ${colors.companycolor};
+  height: auto;
+  width: 100%;
+  padding-top: 40px;
+  color: #000;
+  text-align: center;
+  overflow: hidden;
+`;
+
+export const FooterLinks = styled.div`
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+
+    li {
+      margin: 0 10px;
+
+      a {
+        text-decoration: none;
+        padding: 10px;
+        color: #fff;
+      }
+    }
+  }
+`;
+
+export const SocialMedia = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  a {
+    margin: 0 10px;
+
+    svg {
+      color: #fff;
+      transition: all 0.2s ease-in-out;
+
+      &:hover {
+        transform: scale(1.2);
+        color: #aa7d03;
+      }
+    }
+  }
+`;
--- my-app/src/pages/Footer.tsx
+++ my-app/src/pages/Footer.tsx
@@ -8,75 +8,75 @@ import {
   SnapchatLogo,
   LinkedinLogo,
 } from "phosphor-react";
+import { colors } from "./general/constants";
+import { FooterContainer, FooterLinks, SocialMedia } from "./Footer.styles";
 
 function Footer(): JSX.Element {
   return (
-    <footer className="footer">
-      <div className="container">
-        <div className="footer-links">
-          <ul>
-            <li>
-              <a href="#top">Zum Seitenanfang</a>
-            </li>
-            <li>
-              <Link to="/Datenschutzerklaerung">Datenschutzerklärung</Link>
-            </li>
-            <li>
-              <Link to="/Impressum">Impressum</Link>
-            </li>
-          </ul>
-        </div>
-        <div className="social-media">
-          <a
-            href="https://twitter.com/deliverybreak"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <TwitterLogo size={24} />
-          </a>
-          <a
-            href="https://www.facebook.com/profile.php?id=100091669221469&sk=about"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FacebookLogo size={24} />
-          </a>
-          <a
-            href="https://www.instagram.com/delivery_breakfast_"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <InstagramLogo size={24} />
-          </a>
-          <a
-            href="https://www.tiktok.com/@delivery_breakfast?is_from_webapp=1&sender_device=pc"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <TiktokLogo size={24} />
-          </a>
-          <a
-            href="https://t.snapchat.com/E0HWtcis"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <SnapchatLogo size={24} />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/delivery-breakfast-723b46272"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <LinkedinLogo size={24} />
-          </a>
+    <FooterContainer>
+      <SocialMedia>
+        <a
+          href="https://twitter.com/deliverybreak"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <TwitterLogo size={24} />
+        </a>
+        <a
+          href="https://www.facebook.com/profile.php?id=100091669221469&sk=about"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FacebookLogo size={24} />
+        </a>
+        <a
+          href="https://www.instagram.com/delivery_breakfast_"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <InstagramLogo size={24} />
+        </a>
+        <a
+          href="https://www.tiktok.com/@delivery_breakfast?is_from_webapp=1&sender_device=pc"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <TiktokLogo size={24} />
+        </a>
+        <a
+          href="https://t.snapchat.com/E0HWtcis"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <SnapchatLogo size={24} />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/delivery-breakfast-723b46272"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <LinkedinLogo size={24} />
+        </a>
+      </SocialMedia>
+      <FooterLinks>
+        <ul>
           <li>
-            <Link to="/">
-              &copy; delivery-breakfast {new Date().getFullYear()}
-            </Link>
+            <a href="#top">Nach oben</a>
           </li>
-        </div>
+          <li>
+            <Link to="/Datenschutzerklaerung">Datenschutz</Link>
+          </li>
+          <li>
+            <Link to="/Impressum">Impressum</Link>
+          </li>
+        </ul>
+      </FooterLinks>
+      <div style={{ textAlign: "center", marginTop: "1rem" }}>
+        <Link to="/" style={{ color: "white", textDecoration: "none" }}>
+          &copy; delivery-breakfast {new Date().getFullYear()}
+        </Link>
       </div>
-    </footer>
+    </FooterContainer>
   );
 }
 
