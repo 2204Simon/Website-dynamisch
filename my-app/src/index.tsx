751674a6f4d1c84325114e1a06b15a44fb1f050e
kargmattis <willykarg@web.de>
2023-04-24 18:49:58 +0200

    delete unused variables

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -3,8 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
-import Test from "./test";
-import combinedStore from "./redux/UserData";
+import combinedStore from "./redux/RootStore";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
@@ -13,13 +12,7 @@ root.render(
     <Provider store={combinedStore}>
       <App />
     </Provider>
-    <Provider store={combinedStore}>
-      <Test />
-    </Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
