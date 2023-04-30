3a78d38f54a436cf2dff76586764e38dd8990f0f
ThorstenLicht <114730064+ThorstenLicht@users.noreply.github.com>
2023-04-30 15:27:46 +0200

    Optimierung Produkte und Mausaktivitätserkennung

--- my-app/src/pages/produkte/Produkt.tsx
+++ my-app/src/pages/produkte/Produkt.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ShoppingCard from "./ShoppingCard";
 import Logo from "../../img/Logo.webp";
 import BaguetteAlone from "../../img/Food/BaguetteAlone.webp";
@@ -29,8 +29,111 @@ import OSaftUndOrangenbrot from "../../img/Food/OSaftUndOrangenbrot.webp";
 import ScrollContainer from "./Arrows";
 
 function Produkt() {
-  const showScrollContainer = window.innerWidth >= 1024;
-  const isMaxWidth1024 = window.matchMedia("(max-width: 1024px)").matches;
+  const ShoppingCardFood = () => (
+    <div style={{ display: "flex", flexWrap: "nowrap" }}>
+      <ShoppingCard title={"Baguette"} price={2.99} image={BaguetteAlone} />
+      <ShoppingCard
+        title={"Käsebaguette"}
+        price={3.59}
+        image={BaguetteYellow}
+      />
+      <ShoppingCard title={"Brezel"} price={1.49} image={Brezel} />
+      <ShoppingCard title={"Brötchen"} price={1.29} image={Brötchen} />
+      <ShoppingCard title={"Croissant"} price={1.79} image={Croissant} />
+      <ShoppingCard
+        title={"DB Fresh Chicken"}
+        price={3.79}
+        image={HamburgerBrötchen}
+      />
+      <ShoppingCard title={"Toast"} price={3.59} image={Toast} />
+      <ShoppingCard
+        title={"Toast mit Käse"}
+        price={1.79}
+        image={BelegtesBrötchen}
+      />
+      <ShoppingCard
+        title={"Baguette mit Käse"}
+        price={3.59}
+        image={KäseBaguette}
+      />
+    </div>
+  );
+  const ShoppingCardTrinks = () => (
+    <div style={{ display: "flex", flexWrap: "nowrap" }}>
+      <ShoppingCard title={"Espresso"} price={1.99} image={Espresso} />
+      <ShoppingCard title={"Kaffee"} price={5.99} image={Kaffee} />
+      <ShoppingCard
+        title={"Latte Machiatto"}
+        price={5.99}
+        image={LatteMachiatto}
+      />
+      <ShoppingCard title={"Orangensaft"} price={5.99} image={Orangensaft} />
+      <ShoppingCard title={"Früchtetee"} price={5.99} image={Früchtetee} />
+      <ShoppingCard title={"Wasser"} price={5.99} image={Wasser} />
+      <ShoppingCard title={"Zitronentee"} price={5.99} image={Zitronentee} />
+      <ShoppingCard
+        title={"Heiße Schokolade"}
+        price={5.99}
+        image={HeißeSchokolade}
+      />
+    </div>
+  );
+
+  const ShoppingCardMenu = () => (
+    <div style={{ display: "flex", flexWrap: "nowrap" }}>
+      <ShoppingCard
+        title={"Bayrisches Menü"}
+        price={7.99}
+        image={Bayrisches_Essen}
+      />
+      <ShoppingCard title={"Türkisches Menemen"} price={7.99} image={Menemen} />
+      <ShoppingCard
+        title={"Wecken mit Kaffee"}
+        price={6.99}
+        image={BrötchenMitKaffe}
+      />
+      <ShoppingCard title={"Obstmenü"} price={5.99} image={BrotEiTeeUndObst} />
+      <ShoppingCard
+        title={"Eiermenü"}
+        price={5.99}
+        image={EigetränkUndEierbrot}
+      />
+      <ShoppingCard
+        title={"klassische Menü"}
+        price={5.99}
+        image={KaffeeMitEiUndBrot}
+      />
+      <ShoppingCard
+        title={"Veggiemenü"}
+        price={5.99}
+        image={OSaftMitFrüchtebrot}
+      />
+      <ShoppingCard
+        title={"Orangenmenü"}
+        price={5.99}
+        image={OSaftUndOrangenbrot}
+      />
+    </div>
+  );
+  const [mouseActive, setMouseActive] = useState(false);
+
+  useEffect(() => {
+    function handleMouseMove() {
+      setMouseActive(true);
+    }
+
+    function handleMouseLeave() {
+      setMouseActive(false);
+    }
+
+    window.addEventListener("mousemove", handleMouseMove);
+    window.addEventListener("mouseleave", handleMouseLeave);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+      window.removeEventListener("mouseleave", handleMouseLeave);
+    };
+  }, []);
   return (
     <>
       <h2>Unsere Produkte</h2>
@@ -43,213 +146,41 @@ function Produkt() {
       </p>
 
       <h3>Speisen</h3>
-      {showScrollContainer && !isMaxWidth1024 ? (
+      {mouseActive ? (
         <ScrollContainer scrollAmount={300}>
-          <ShoppingCard title={"Baguette"} price={2.99} image={BaguetteAlone} />
-          <ShoppingCard
-            title={"Käsebaguette"}
-            price={3.59}
-            image={BaguetteYellow}
-          />
-          <ShoppingCard title={"Brezel"} price={1.49} image={Brezel} />
-          <ShoppingCard title={"Brötchen"} price={1.29} image={Brötchen} />
-          <ShoppingCard title={"Croissant"} price={1.79} image={Croissant} />
-          <ShoppingCard
-            title={"DB Fresh Chicken"}
-            price={3.79}
-            image={HamburgerBrötchen}
-          />
-          <ShoppingCard title={"Toast"} price={3.59} image={Toast} />
-          <ShoppingCard
-            title={"Toast mit Käse"}
-            price={1.79}
-            image={BelegtesBrötchen}
-          />
-          <ShoppingCard
-            title={"Baguette mit Käse"}
-            price={3.59}
-            image={KäseBaguette}
-          />
+          <ShoppingCardFood />
         </ScrollContainer>
       ) : (
         <div
           style={{ display: "flex", flexWrap: "nowrap", overflowX: "scroll" }}
         >
-          <ShoppingCard title={"Baguette"} price={2.99} image={BaguetteAlone} />
-          <ShoppingCard
-            title={"Käsebaguette"}
-            price={3.59}
-            image={BaguetteYellow}
-          />
-          <ShoppingCard title={"Brezel"} price={1.49} image={Brezel} />
-          <ShoppingCard title={"Brötchen"} price={1.29} image={Brötchen} />
-          <ShoppingCard title={"Croissant"} price={1.79} image={Croissant} />
-          <ShoppingCard
-            title={"DB Fresh Chicken"}
-            price={3.79}
-            image={HamburgerBrötchen}
-          />
-          <ShoppingCard title={"Toast"} price={3.59} image={Toast} />
-          <ShoppingCard
-            title={"Toast mit Käse"}
-            price={1.79}
-            image={BelegtesBrötchen}
-          />
-          <ShoppingCard
-            title={"Baguette mit Käse"}
-            price={3.59}
-            image={KäseBaguette}
-          />
+          <ShoppingCardFood />
         </div>
       )}
 
       <h3>Getränke</h3>
-      {showScrollContainer && !isMaxWidth1024 ? (
+      {mouseActive ? (
         <ScrollContainer scrollAmount={300}>
-          <ShoppingCard title={"Espresso"} price={1.99} image={Espresso} />
-          <ShoppingCard title={"Kaffee"} price={5.99} image={Kaffee} />
-          <ShoppingCard
-            title={"Latte Machiatto"}
-            price={5.99}
-            image={LatteMachiatto}
-          />
-          <ShoppingCard
-            title={"Orangensaft"}
-            price={5.99}
-            image={Orangensaft}
-          />
-          <ShoppingCard title={"Früchtetee"} price={5.99} image={Früchtetee} />
-          <ShoppingCard title={"Wasser"} price={5.99} image={Wasser} />
-          <ShoppingCard
-            title={"Zitronentee"}
-            price={5.99}
-            image={Zitronentee}
-          />
-          <ShoppingCard
-            title={"Heiße Schokolade"}
-            price={5.99}
-            image={HeißeSchokolade}
-          />
+          <ShoppingCardTrinks />
         </ScrollContainer>
       ) : (
         <div
           style={{ display: "flex", flexWrap: "nowrap", overflowX: "scroll" }}
         >
-          <ShoppingCard title={"Espresso"} price={1.99} image={Espresso} />
-          <ShoppingCard title={"Kaffee"} price={5.99} image={Kaffee} />
-          <ShoppingCard
-            title={"Latte Machiatto"}
-            price={5.99}
-            image={LatteMachiatto}
-          />
-          <ShoppingCard
-            title={"Orangensaft"}
-            price={5.99}
-            image={Orangensaft}
-          />
-          <ShoppingCard title={"Früchtetee"} price={5.99} image={Früchtetee} />
-          <ShoppingCard title={"Wasser"} price={5.99} image={Wasser} />
-          <ShoppingCard
-            title={"Zitronentee"}
-            price={5.99}
-            image={Zitronentee}
-          />
-          <ShoppingCard
-            title={"Heiße Schokolade"}
-            price={5.99}
-            image={HeißeSchokolade}
-          />
+          <ShoppingCardTrinks />
         </div>
       )}
 
       <h3>Menüs</h3>
-      {showScrollContainer && !isMaxWidth1024 ? (
+      {mouseActive ? (
         <ScrollContainer scrollAmount={300}>
-          <ShoppingCard
-            title={"Bayrisches Menü"}
-            price={7.99}
-            image={Bayrisches_Essen}
-          />
-          <ShoppingCard
-            title={"Türkisches Menemen"}
-            price={7.99}
-            image={Menemen}
-          />
-          <ShoppingCard
-            title={"Wecken mit Kaffee"}
-            price={6.99}
-            image={BrötchenMitKaffe}
-          />
-          <ShoppingCard
-            title={"Obstmenü"}
-            price={5.99}
-            image={BrotEiTeeUndObst}
-          />
-          <ShoppingCard
-            title={"Eiermenü"}
-            price={5.99}
-            image={EigetränkUndEierbrot}
-          />
-          <ShoppingCard
-            title={"klassische Menü"}
-            price={5.99}
-            image={KaffeeMitEiUndBrot}
-          />
-          <ShoppingCard
-            title={"Veggiemenü"}
-            price={5.99}
-            image={OSaftMitFrüchtebrot}
-          />
-          <ShoppingCard
-            title={"Orangenmenü"}
-            price={5.99}
-            image={OSaftUndOrangenbrot}
-          />
+          <ShoppingCardMenu />
         </ScrollContainer>
       ) : (
         <div
           style={{ display: "flex", flexWrap: "nowrap", overflowX: "scroll" }}
         >
-          <ShoppingCard
-            title={"Bayrisches Menü"}
-            price={7.99}
-            image={Bayrisches_Essen}
-          />
-          <ShoppingCard
-            title={"Türkisches Menemen"}
-            price={7.99}
-            image={Menemen}
-          />
-          <ShoppingCard
-            title={"Wecken mit Kaffee"}
-            price={6.99}
-            image={BrötchenMitKaffe}
-          />
-          <ShoppingCard
-            title={"Obstmenü"}
-            price={5.99}
-            image={BrotEiTeeUndObst}
-          />
-          <ShoppingCard
-            title={"Eiermenü"}
-            price={5.99}
-            image={EigetränkUndEierbrot}
-          />
-          <ShoppingCard
-            title={"klassische Menü"}
-            price={5.99}
-            image={KaffeeMitEiUndBrot}
-          />
-          <ShoppingCard
-            title={"Veggiemenü"}
-            price={5.99}
-            image={OSaftMitFrüchtebrot}
-          />
-          <ShoppingCard
-            title={"Orangenmenü"}
-            price={5.99}
-            image={OSaftUndOrangenbrot}
-          />
+          <ShoppingCardMenu />
         </div>
       )}
     </>
