340738f0f5c547e0189a79d32a649359327be912
JoHerr02 <72371985+JoHerr02@users.noreply.github.com>
2023-05-10 20:47:43 +0200

    Weiterbau Startseite

--- my-app/src/pages/home/Home.tsx
+++ my-app/src/pages/home/Home.tsx
@@ -1,158 +1,40 @@
-//import React from "react";
-import { Link } from "react-router-dom";
-import fakeZeitung from "../.././img/test1.webp";
-import hintergrund_dunkel from "../.././img/Hintergrund_Croissant_verdunkelt.webp";
-import { OrangeButton } from "../general/button";
+import { Link } from 'react-router-dom';
+import { OrangeButton } from '../general/button';
+import { Cooperation, Produktteaser, Image, ImageBox, Bg, Dunkel } from './styles/Home.styles';
+import fakeZeitung from "../.././img/Fake_Zeitung.webp";
 
-// const Home: React.FC = (): JSX.Element => {
-//   return (
-//     <div className="App">
-//       <Chatra />
-//       <main className="DunklerHintergrund">
-//         {/* <div className="Anfangssatz">  */}
-//         <div>
-//           <h2>Stelle Dir Dein perfektes Frühstück zusammen</h2>
-//           <div>
-//             <Link to="/Produkte">
-//               <OrangeButton caption={"Zu unseren Produkten"}></OrangeButton>
-//             </Link>
-//           </div>
-//         </div>
-//         {/* <div className="parallax"> */}
-//         <div>
-//           {/* <div className="parallax-content"> */}
-
-//             <h1>Unsere Kooperation</h1>
-//             <img
-//               src={fakeZeitung}
-//               alt="Logo von FakeZeitung"
-//               width="200"
-//               height="200"
-//             />
-//             <p>
-//               Wir haben seit 2001 eine Kooperation mit der Zeitungsfirma
-//               FakeZeitung.
-//               <br />
-//               Mit Hilfe der Zeitungsboten kommt Dein Frühstück zu Dir. Dadurch
-//               können wir kostengünstig und emissionsarm liefern!
-//             </p>
-// </div>
-//           {/* </div>
-//         </div> */}
-//       </main>
-//     </div>
-//   );
-// };
-
-// export default Home;
-
-import React, { useEffect, useState } from "react";
-import {
-  Cooperation,
-  Produktteaser,
-  Image,
-  ImageBox,
-} from "./styles/Home.styles";
-
-// Definiere ein Interface für die Konfiguration der Startseite
-interface HomePageConfig {
-  backgroundColor: string;
-  textColor: string;
-}
-
-// Definiere die Standardkonfiguration
-const defaultConfig: HomePageConfig = {
-  backgroundColor: "white",
-  textColor: "black",
-};
-
-// Definiere die Komponente für die Startseite
 const Home: React.FC = () => {
-  const [config, setConfig] = useState(defaultConfig);
-
-  // Definiere die Funktion, die beim Scrollen aufgerufen wird
-  const onScroll = () => {
-    const body = document.querySelector("body");
-    const scrollTop =
-      document.documentElement.scrollTop || document.body.scrollTop;
-    const scrollHeight =
-      document.documentElement.scrollHeight || document.body.scrollHeight;
-    const windowHeight = window.innerHeight;
-    const threshold = scrollHeight - windowHeight;
-    const newConfig = getConfig(scrollTop, threshold);
-    setConfig(newConfig);
-  };
-
-  // Definiere eine Funktion, um die Konfiguration basierend auf der Scroll-Position zu aktualisieren
-  const getConfig = (scrollTop: number, threshold: number): HomePageConfig => {
-    const progress = scrollTop / threshold;
-    const backgroundColor = `rgb(${progress * 255}, ${progress * 255}, ${
-      progress * 255
-    })`;
-    const textColor = `rgb(${255 - progress * 255}, ${255 - progress * 255}, ${
-      255 - progress * 255
-    })`;
-    return {
-      backgroundColor,
-      textColor,
-    };
-  };
-
-  useEffect(() => {
-    // Füge den Event-Listener für das Scroll-Ereignis hinzu
-    window.addEventListener("scroll", onScroll);
-    // Entferne den Event-Listener beim Entladen der Komponente
-    return () => window.removeEventListener("scroll", onScroll);
-  }, []);
-
   return (
     <>
-    
-  
+    <Bg>
+    <Dunkel>
 
-      <div
-        style={{
-          backgroundColor: config.backgroundColor,
-          color: config.textColor,
-        }}
-      >
+      <Produktteaser>
+        <div>
+        <h2>Stelle Dir Dein perfektes Frühstück zusammen</h2>
+        <Link to="/Produkte">
+          <OrangeButton caption={'Zu unseren Produkten'} />
+        </Link>
+        </div>
+      </Produktteaser>
 
-<ImageBox>
-    <Image src={hintergrund_dunkel} alt="LogoFakezeitung" />
-        {/* Hier kommt der Inhalt der Startseite */}
-
-        <Produktteaser>
-          <h2 style={{ color: config.textColor }}>
-            Stelle Dir Dein perfektes Frühstück zusammen
-          </h2>
-          <Link to="/Produkte">
-            {/* <LandingPageButton 
-              caption={"Zu unseren Produkten"} 
-              color={config.textColor}
-              ></LandingPageButton> */}
-            <OrangeButton caption={"Zu unseren Produkten"} />
-          </Link>
-        </Produktteaser>
-
-        <Cooperation>
-          <h1 style={{ color: config.textColor }}>Unsere Kooperation</h1>
-          <ImageBox>
-            <Image src={fakeZeitung} alt="LogoFakezeitung" />
-          </ImageBox>
-
-          <p style={{ color: config.textColor }}>
-            Wir haben seit 2001 eine Kooperation mit der Zeitungsfirma
-            FakeZeitung.
-            <br />
-            Mit Hilfe der Zeitungsboten kommt Dein Frühstück zu Dir. Dadurch
-            können wir kostengünstig und emissionsarm liefern!
-          </p>
-        </Cooperation>
-
+      <Cooperation>
+        <div>
+        <h1>Unsere Kooperation</h1>
+        <ImageBox>
+          <Image src={fakeZeitung} alt="LogoFakezeitung" />
         </ImageBox>
-
-
+        <p>
+          Wir haben seit 2001 eine Kooperation mit der Zeitungsfirma FakeZeitung.
+          <br />
+          Mit Hilfe der Zeitungsboten kommt Dein Frühstück zu Dir. Dadurch können wir kostengünstig und emissionsarm
+          liefern!
+        </p>
       </div>
+      </Cooperation>
+
+      </Dunkel>
+    </Bg>
     </>
   );
 };
--- my-app/src/pages/home/styles/Home.styles.ts
+++ my-app/src/pages/home/styles/Home.styles.ts
@@ -1,31 +1,58 @@
 import React from "react";
 import styled from "styled-components";
 import { colors, mediaQueries } from "../../general/constants";
-
-
-export const Container = styled.img`
-  position: relative;
-  height: 100vh;
-  background-size: cover;
-  background-repeat: no-repeat;
-`;
+import hintergrund_dunkel from '../../../img/Hintergrund_Croissant_verdunkelt.webp';
 
 
 export const Produktteaser = styled.div`
-  padding-top: 20%;
-  padding-bottom: 20%;
+  // padding-top: 20%;
+  // padding-bottom: 20%;
   text-align: center;
+  color: ${colors.black};
+  height: calc(100vh - 130px);
+  display: flex;
+  justify-content: center; 
+  align-items: center;
 
 
   h2 {
-    text-align: center;
+    // text-align: center;
     padding: 0;
-    // color: ${colors.black};
+    color: ${colors.white};  
+
+
   }
 
   p {
     text-align: center;
     margin-bottom: 1rem;
+    color: ${colors.companycolor};
+  }
+
+`;
+
+export const Cooperation = styled.div`
+  // padding-bottom: 5%;
+  background-color: ${colors.black};
+  height: calc(100vh - 134.5px);
+  display: flex;
+  justify-content: center; 
+  align-items: center;
+
+  
+
+  h1 {
+    text-align: center;
+    color: ${colors.white};
+    color: config.textColor;
+    margin-top: 0px;
+
+  }
+
+  p {
+    text-align: center;
+    padding-bottom: 0;
+    padding-left: 0;
     color: ${colors.companycolor};
   }
 
@@ -36,36 +63,13 @@ export const Produktteaser = styled.div`
   }
 `;
 
-export const Cooperation = styled.div`
-  padding-bottom: 5%;
-  
-
-  h1 {
-    text-align: center;
-    // color: ${colors.black};
-    color: config.textColor;
-  }
-
-  p {
-    text-align: center;
-    margin-bottom: 1rem;
-    // color: ${colors.companycolor};
-  }
-
-  form {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
-`;
-
 export const Image = styled.img`
-  width: 20%;
-  height: auto;
+  // width: 20%;
+  height: 300px;
   margin-left: auto;
   margin-right: auto;
   transition: transform 0.3s ease-in-out;
-  border-radius: 10px;
+  border-radius: 150px;
   border: 2px;
   border-color: ${colors.black};
 `;
@@ -74,7 +78,9 @@ export const ImageBox = styled.div`
 display: flex;
   align-items: center;
   justify-content: center;
-  height: 100%;
+  
+  margin-top: 10px;
+  margin-bottom: 10px;
 `;
 
 export const media = {
@@ -82,4 +88,15 @@ export const media = {
   laptop: "@media(min-width: ${mediaQueries.large})",
 };
 
+export const Dunkel = styled.div`
+background-color: rgba(0, 0, 0, 0.8); /* Hier kannst du die Farbe und Transparenz anpassen */
+`;
 
+export const Bg = styled.div`
+background-image: url(${hintergrund_dunkel});
+background-attachment: fixed;
+background-position: center;
+background-repeat: no-repeat;
+background-size: cover;
+
+`;
