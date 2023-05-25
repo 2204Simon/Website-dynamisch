a4782c1829636e79a9695df0de1660356940a619
2204Simon <sigl.simon@web.de>
2023-05-25 20:29:38 +0200

    1000px popup

--- my-app/src/pages/bestellung/SideBarBuy.tsx
+++ my-app/src/pages/bestellung/SideBarBuy.tsx
@@ -154,7 +154,7 @@ export default function SideBarBuy({ price }: SideBarProps): JSX.Element {
             className="scrollable-popup"
             style={{
               overflowY: "scroll",
-              maxHeight: "600px",
+              maxHeight: "1000px",
             }}
           >
             {!loggedIn ? (
