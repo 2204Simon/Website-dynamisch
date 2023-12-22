b1799eaa2c74fed1e8a3b0fd77f1919537ebc6e7
2204Simon <sigl.simon@web.de>
2023-12-22 11:21:33 +0100

    Refactor Zeitung component and add styles

--- my-app/src/pages/zeitung/Zeitung.tsx
+++ my-app/src/pages/zeitung/Zeitung.tsx
@@ -1,16 +1,69 @@
-import { Link } from "react-router-dom";
-import { OrangeButton } from "../general/button";
+// Zeitung.tsx
 
-const Zeitung: React.FC = () => {
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import {
+  ZeitungContainer,
+  SubscriptionSection,
+  FaqSection,
+  FaqList,
+  FaqItem,
+  FaqDetails,
+  FaqSummary,
+} from "./styles/Zeitung.styles";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const Zeitung = () => {
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+
+  const faqs = [
+    {
+      question: "Wie kann ich ein Abonnement abschließen?",
+      answer:
+        "Sie können ein Abonnement online über unsere Website abschließen.",
+    },
+    {
+      question: "Wann wird meine Zeitung geliefert?",
+      answer: "Die Zeitung wird jeden Morgen um 6 Uhr geliefert.",
+    },
+    // Füge weitere FAQs hinzu
+  ];
+
+  const handleDateChange = (date: Date | null) => {
+    setSelectedDate(date);
+
+    if (!date) {
+      toast.error("Bitte wählen Sie ein Datum aus.");
+    }
+  };
+
   return (
-    <>
-      <div className="zeitung">
-        <div className="zeitung__title">
-          <h1>Zeitung</h1>
-        </div>
-        <p>xy</p>
-      </div>
-    </>
+    <ZeitungContainer>
+      <SubscriptionSection>
+        <h2>Abonnement auswählen</h2>
+        <label>Datum auswählen:</label>
+        <DatePicker selected={selectedDate} onChange={handleDateChange} />
+        {/* Hier kannst du den Code für die Auswahl des Abonnements hinzufügen */}
+      </SubscriptionSection>
+
+      <FaqSection>
+        <h2>FAQs</h2>
+        <FaqList>
+          {faqs.map((faq, index) => (
+            <FaqItem key={index}>
+              <FaqDetails>
+                <FaqSummary>{faq.question}</FaqSummary>
+                <p>{faq.answer}</p>
+              </FaqDetails>
+            </FaqItem>
+          ))}
+        </FaqList>
+      </FaqSection>
+
+      <ToastContainer />
+    </ZeitungContainer>
   );
 };
 
--- my-app/src/pages/zeitung/styles/Zeitung.styles.tsx
+++ my-app/src/pages/zeitung/styles/Zeitung.styles.tsx
@@ -1,3 +1,51 @@
+// Zeitung.styles.js
+
 import styled from "styled-components";
 
-export const Bg = styled.div``;
+export const ZeitungContainer = styled.div`
+  display: flex;
+  flex-direction: column; /* Änderung auf Spaltenlayout für Mobilgeräte */
+  padding: 20px;
+
+  /* Tablets und größere Geräte */
+  @media screen and (min-width: 768px) {
+    flex-direction: row;
+  }
+
+  /* Desktop-Bildschirme und größere Geräte */
+  @media screen and (min-width: 1200px) {
+    padding: 40px;
+  }
+`;
+
+export const SubscriptionSection = styled.div`
+  margin-bottom: 20px;
+  flex: 1; /* Füge Flex-Grow für Responsive-Layout hinzu */
+`;
+
+export const FaqSection = styled.div`
+  margin-bottom: 20px;
+  flex: 1; /* Füge Flex-Grow für Responsive-Layout hinzu */
+`;
+
+export const FaqList = styled.ul`
+  list-style: none;
+  padding: 0;
+`;
+
+export const FaqItem = styled.li`
+  margin-bottom: 15px;
+`;
+
+export const FaqDetails = styled.details`
+  cursor: pointer;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: 10px;
+`;
+
+export const FaqSummary = styled.summary`
+  font-weight: bold;
+`;
+
+/* Media Queries für die responsive Gestaltung */
