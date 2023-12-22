51da22b2ba43ea71034d618e8deb7e89fdb12b55
2204Simon <sigl.simon@web.de>
2023-12-22 15:14:52 +0100

    Update PayPal client ID in .env

--- my-app/src/pages/PaypalPayment.tsx
+++ my-app/src/pages/PaypalPayment.tsx
@@ -9,8 +9,7 @@ export function PayPalPayment() {
     <PayPalScriptProvider
       options={
         {
-          clientId:
-            "AVyT9gzsIvUM4isPoCk8F2U1aqr2X92U2ETBtHQJtAr2IqcNzlV7DKGDni0wNLjmqZV5rnpf2HUOKO4C",
+          clientId: process.env.REACT_APP_PAYPAL_CLIENT_ID as string,
         } as ReactPayPalScriptOptions
       }
     >
