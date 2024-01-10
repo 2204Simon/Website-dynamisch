5cef6d285a0d06ec82fcfef2f2edce3461b45c56
JoHerr02 <72371985+JoHerr02@users.noreply.github.com>
2024-01-10 19:33:10 +0100

    CSS Cookie-Meldung

--- my-app/src/CookieBanner.tsx
+++ my-app/src/CookieBanner.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import "./App.css";
-import "./pages/general/button.styles";
+import { CookieBannerStyle } from "./Cookiebanner.styles";
+import { Button } from "./pages/general/button.styles";
 
 interface CookieBannerProps {
   onAccept: () => void;
@@ -11,16 +11,19 @@ export const CookieBanner: React.FC<CookieBannerProps> = ({
   onAccept,
   onDecline,
 }) => (
-  <div>
-    <p>
-      Wir verwenden Cookies, um Ihre Erfahrung zu verbessern. Durch die Nutzung
-      unserer Website stimmen Sie allen Cookies gemäß unserer Cookie-Richtlinie
-      zu.
-    </p>
-    <button onClick={onAccept}>Akzeptieren</button>
-    <button onClick={onDecline}>Ablehnen</button>
-  </div>
+  <CookieBannerStyle>
+    <div className="cookiebanner">
+      <div className="cookietext">
+        Wir verwenden Cookies, um Ihre Erfahrung zu verbessern. Durch die
+        Nutzung unserer Website stimmen Sie allen Cookies gemäß unserer
+        Cookie-Richtlinie zu.
+      </div>
+      <Button className="white-orange" onClick={onAccept}>
+        Akzeptieren
+      </Button>
+      <Button className="white-orange" onClick={onDecline}>
+        Ablehnen
+      </Button>
+    </div>
+  </CookieBannerStyle>
 );
-
-//<BlackColorButton onClick={onAccept}>Akzeptieren</BlackColorButton>
-  //  <BlackColorButton onClick={onDecline}>Ablehnen</BlackColorButton>
