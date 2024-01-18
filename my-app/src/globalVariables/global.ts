1bafc6a1e37cc6d14552ef13da8332f4cd138bfd
2204Simon <sigl.simon@web.de>
2024-01-18 20:42:39 +0100

    Update baseUrl in global.ts file

--- my-app/src/globalVariables/global.ts
+++ my-app/src/globalVariables/global.ts
@@ -1,5 +1,4 @@
-//export const baseUrl ="https://delivery-breakfast.projekt.dhbw-heidenheim.de:3001/api/v1";
-
+//kann auch bei push auf Server auf localhost bleiben
 export const baseUrl = "http://localhost:3001/api/v1";
 
 export const KUNDEN_ID = "kundenId";
