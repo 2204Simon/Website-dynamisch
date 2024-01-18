6c6e1bf749e9f27c46e73baff8d8c7bb434de544
kargmattis <115991382+kargmattis@users.noreply.github.com>
2024-01-18 17:53:47 +0100

    submit funkioniert nicht

--- my-app/src/pages/loggedIn/AdressData.tsx
+++ my-app/src/pages/loggedIn/AdressData.tsx
@@ -18,13 +18,14 @@ import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
 import FormControl from "@mui/material/FormControl";
-import FormControlLabel from "@mui/material/FormControlLabel";
 import { FaPaypal } from "react-icons/fa";
 import { colors } from "../general/constants";
-import { ZeitungsAbo } from "../Zeitungsabo";
-import { Checkbox, FormGroup } from "@mui/material";
+import { FormGroup } from "@mui/material";
 import { addNewAdress } from "../../redux/adressDataReducer";
-import { getRequest } from "../../serverFunctions/generelAPICalls";
+import {
+  getRequest,
+  sendPutRequest,
+} from "../../serverFunctions/generelAPICalls";
 import { useCookies } from "react-cookie";
 import { KUNDEN_ID } from "../../globalVariables/global";
 import { CustomToast } from "../general/toast.style";
@@ -83,7 +84,8 @@ export default function AdressInformation(): JSX.Element {
     setEditMode(false);
   };
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    console.log("submit");
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     const preparedData: AdressData = {
@@ -113,9 +115,17 @@ export default function AdressInformation(): JSX.Element {
       return;
     }
     console.log(preparedData);
-    dispatch(addNewAdress(preparedData));
-    setEditedData(null);
-    setEditMode(false);
+    try {
+      const putAdressData = await sendPutRequest("/adresse", preparedData);
+      const putPaymentData = await sendPutRequest("/zahlung", paymentData);
+      dispatch(addNewAdress(putAdressData));
+      dispatch(addPayment(putPaymentData));
+      setEditedData(null);
+      setEditMode(false);
+    } catch (error) {
+      CustomToast.error("Fehler beim Speichern der Daten");
+      console.log(error);
+    }
   };
   const [hasSubscription, setHasSubscription] = useState(false);
   const [expiryDate, setExpiryDate] = useState(new Date());
@@ -124,9 +134,9 @@ export default function AdressInformation(): JSX.Element {
     <Container>
       <Card>
         {editMode ? (
-          <form onSubmit={handleSubmit}>
+          <form onSubmit={e => handleSubmit(e)}>
             <Grid justifyContent={"center"}>
-              <Title>Adresse:</Title>
+              <Title>Adresse</Title>
               <TextField
                 fullWidth
                 required
@@ -198,14 +208,14 @@ export default function AdressInformation(): JSX.Element {
                   },
                 }}
               />
-
-              <Title>Zeitungsabonnement:</Title>
-              <ZeitungsAbo
+              {/* 
+              <Title>Zeitungsabonnement:</Title> */}
+              {/* <ZeitungsAbo
                 hasSubscription={hasSubscription}
                 setHasSubscription={setHasSubscription}
                 expiryDate={expiryDate}
                 setExpiryDate={setExpiryDate}
-              />
+              /> */}
               <Grid item xs={12}>
                 <Title>Zahlungsmöglichkeiten: </Title>
                 <FormControl component="fieldset">
@@ -221,6 +231,7 @@ export default function AdressInformation(): JSX.Element {
                         id="paypalEmail"
                         label="PayPal Email"
                         name="paypalEmail"
+                        defaultValue={paymentInformation.paypalEmail}
                         inputProps={{
                           maxLength: 100,
                         }}
@@ -242,6 +253,7 @@ export default function AdressInformation(): JSX.Element {
                         id="bankName"
                         label="Bankname"
                         name="bankName"
+                        defaultValue={paymentInformation.bankname}
                         inputProps={{
                           maxLength: 50,
                         }}
@@ -260,6 +272,7 @@ export default function AdressInformation(): JSX.Element {
                         id="bic"
                         label="BIC"
                         name="bic"
+                        defaultValue={paymentInformation.bic}
                         inputProps={{
                           maxLength: 50,
                         }}
@@ -294,10 +307,7 @@ export default function AdressInformation(): JSX.Element {
               </Grid>
             </Grid>
 
-            <Button
-              style={{ color: colors.companycolor }}
-              onClick={() => handleEdit(adressInformation)}
-            >
+            <Button style={{ color: colors.companycolor }} type="submit">
               Edit
             </Button>
 
