47a5eb24320eb79dec49debdc1772aabf20d48e9
kargmattis <115991382+kargmattis@users.noreply.github.com>
2024-01-19 23:03:49 +0100

    Update baseUrl in global.ts

--- my-app/src/globalVariables/global.ts
+++ my-app/src/globalVariables/global.ts
@@ -1,6 +1,6 @@
-//export const baseUrl = "http://localhost:3001/api/v1";
+export const baseUrl = "http://localhost:3001/api/v1";
 
-export const baseUrl =
-  "http://delivery-breakfast.projekt.dhbw-heidenheim.de:3001/api/v1";
+// export const baseUrl =
+//   "http://delivery-breakfast.projekt.dhbw-heidenheim.de:3001/api/v1";
 
 export const KUNDEN_ID = "kundenId";
