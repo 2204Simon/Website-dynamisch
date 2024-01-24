5c2ad3f76c228337ea5a38ff295e45a9b76f9023
JoHerr02 <72371985+JoHerr02@users.noreply.github.com>
2024-01-24 22:25:53 +0100

    Update Cookiebanner.styles.ts

--- my-app/src/Cookiebanner.styles.ts
+++ my-app/src/Cookiebanner.styles.ts
@@ -9,25 +9,28 @@ export const CookieBannerStyle = styled.div`
     align-items: center;
     position: fixed;
     bottom: 0;
-    width: 100vw; /* Aktualisierte Breite auf 100% */
+    width: 96vh /* Aktualisierte Breite auf 100% */
     height: 100px;
     background-color: ${colors.black};
     z-index: 10;
     padding: 16px 16px 16px 16px;
-  }
+    border-radius: 10px;
+    align-self: center;
+   }
 
   .cookietext {
     font-family: "Montserrat", sans-serif;
     color: #aa7d03;
     font-size: 1.25rem;
+    text-align: center;
   }
 
   @media (max-width: ${mediaQueries.medium}) {
     .cookiebanner {
       justify-content: center;
       flex-direction: column;
-      height: 300px;
-      width: 90vw; /* Aktualisierte Breite auf 100% */
+      //height: 300px;
+      width: 90vw; 
       text-align: center;
 
       padding-right: 85px; //Ausrichtung Icons wegen Chatbot
