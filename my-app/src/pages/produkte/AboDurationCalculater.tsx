249f25285f446c2d1aaf59255909e71363647052
ThorstenLicht <114730064+ThorstenLicht@users.noreply.github.com>
2024-02-09 08:37:25 +0100

    Abonnement Ende

--- my-app/src/pages/produkte/AboDurationCalculater.tsx
+++ my-app/src/pages/produkte/AboDurationCalculater.tsx
@@ -4,12 +4,35 @@ import { getRequest } from "../../serverFunctions/generelAPICalls";
 import { CustomToast } from "../general/toast.style";
 import { useCookies } from "react-cookie";
 import { KUNDEN_ID } from "../../globalVariables/global";
+import { formatGermanDate } from "../../DateUtils";
 
 //TODO Not Working Currently
 function AboDurationCalculator() {
+  interface ProductInformation {
+    produktId: string;
+    titel: string;
+    preis: number;
+    bestellmenge: number;
+    createdAt: string;
+  }
+
+  interface Intervall {
+    start: number;
+    end: number;
+  }
+
   const [cookies] = useCookies([KUNDEN_ID]);
   const [bestellungen, setBestellungen] =
     useState<Array<BestellungsInformation> | null>([]);
+  const [date, setDate] = useState<string | null>(null);
+
+  //Testzweck
+  function millisekundenZuDatum(intervalle: Array<Intervall>) {
+    return intervalle.map(interval => ({
+      start: new Date(interval.start),
+      end: new Date(interval.end),
+    }));
+  }
 
   useEffect(() => {
     const fetchData = async () => {
@@ -29,99 +52,135 @@ function AboDurationCalculator() {
         CustomToast.error("Fehler beim Laden der Daten");
       }
     };
-
     fetchData();
   }, []);
 
-  interface ProductInformation {
-    produktId: string;
-    titel: string;
-    preis: number;
-    bestellmenge: number;
-    createdAt: string;
-  }
+  useEffect(() => {
+    if (bestellungen !== null) {
+      console.log(bestellungen);
+      const newspaperArray = getAllNewspaperOrders(bestellungen);
+      console.log(newspaperArray);
+      const intervals = Intervals(newspaperArray);
+      console.log(millisekundenZuDatum(intervals));
+      const sortedintervals = IntervalSorter(intervals);
+      console.log(millisekundenZuDatum(sortedintervals));
+      const endDate = EndDate(sortedintervals);
+      console.log(endDate);
+      setDate(endDate);
+      console.log(date);
+    }
+  }, [bestellungen]);
 
   function getAllNewspaperOrders(
-    orders: BestellungsInformation[] | null
-  ): ProductInformation[] | null {
+    orders: BestellungsInformation[]
+  ): ProductInformation[] {
     const newspaperOrders: ProductInformation[] = [];
-    if (orders !== null) {
-      orders.forEach(order => {
-        const newspaperOrder = order.produktInformationen.find(
-          product => product.titel === "Zeitung"
-        );
-        if (newspaperOrder) {
-          newspaperOrders.push(newspaperOrder);
-        }
-      });
 
-      return newspaperOrders;
-    } else return null;
-  }
-  //TODO
-  interface Intervall {
-    start: number;
-    end: number;
-  }
-  function RemainingDays(
-    subscriptions: Array<ProductInformation> | null
-  ): number | null {
-    if (subscriptions === null) {
-      return null;
-    } else {
-      const StartEndArray: Array<Intervall> = [];
-      subscriptions.forEach(subscription => {
-        const createdAt = new Date(subscription.createdAt);
-        createdAt.setDate(createdAt.getDate() + 1); //Damit das Abo nicht am selben Bestelltag gilt, sondern erst ab den nächsten Tag das Abo startet
-        const orderDay = createdAt.getTime();
-        const duration = new Date(
-          orderDay + subscription.bestellmenge * 24 * 60 * 60 * 1000
-        );
-        const aboEndDay = duration.getTime();
-        const abo: Intervall = {
-          start: orderDay,
-          end: aboEndDay,
-        };
-        StartEndArray.push(abo);
-      });
-      IntervalsToDays(StartEndArray);
-      return null; //remainingDaysCount > 0 ? remainingDaysCount - 1 : 0;
-    }
-  }
-
-  function IntervalsToDays(intervals: Array<Intervall>) {
-    intervals.sort((a, b) => a.start - b.start); // Sortiere nach Startdatum
-    let remainingDaysCount = 0;
-
-    for (let i = 1; i < intervals.length; i++) {
-      const currentInterval = intervals[i];
-      const previousInterval = intervals[i - 1];
-
-      // Überprüfen auf Überlappung
-      if (currentInterval.start < previousInterval.end) {
-        // Verschiebe Startdatum des aktuellen Intervalls
-        currentInterval.start = previousInterval.end;
-
-        // Überprüfen, ob zwischen dem Enddatum des vorherigen Intervalls
-        // und dem Startdatum des aktuellen Intervalls eine Lücke ist
-        if (currentInterval.start > currentInterval.end) {
-          // Es gibt eine Lücke, füge ein neues Objekt ein
-          const freeInterval = {
-            start: previousInterval.end,
-            end: currentInterval.start,
-          };
-          intervals.splice(i, 0, freeInterval);
-        }
+    orders.forEach(order => {
+      const newspaperOrder = order.produktInformationen.find(
+        product => product.titel === "Zeitung"
+      );
+      if (newspaperOrder) {
+        newspaperOrders.push(newspaperOrder);
       }
-    }
+    });
 
+    return newspaperOrders;
+  }
+
+  function Intervals(
+    subscriptions: Array<ProductInformation>
+  ): Array<Intervall> {
+    const intervals: Array<Intervall> = [];
+    subscriptions.forEach(subscription => {
+      const createdAt = new Date(subscription.createdAt);
+      console.log("Hier", createdAt);
+      console.log("Hier2", subscription.createdAt);
+      createdAt.setDate(createdAt.getDate() + 1); //Damit das Abo nicht am selben Bestelltag gilt, sondern erst ab den nächsten Tag das Abo startet
+      const orderDay = createdAt.getTime();
+      const duration = new Date(
+        orderDay + subscription.bestellmenge * 24 * 60 * 60 * 1000
+      );
+      const aboEndDay = duration.getTime();
+      const abo: Intervall = {
+        start: orderDay,
+        end: aboEndDay,
+      };
+      intervals.push(abo);
+    });
     return intervals;
   }
 
-  const newspaperOrder = getAllNewspaperOrders(bestellungen);
-  console.log(RemainingDays(newspaperOrder));
-  //TODO Ende
-  return <p>Dein Abonnement läuft noch bis zum </p>;
+  function IntervalSorter(intervals: Array<Intervall>) {
+    //Falls Array weniger als einen Eintrag hat, wird diese Funktion übersprungen
+    if (intervals.length > 1) {
+      intervals.sort((a, b) => a.start - b.start); // Sortiere nach Startdatum
+      for (let i = 1; i < intervals.length; i++) {
+        const currentInterval = intervals[i];
+        const previousInterval = intervals[i - 1];
+        // Überprüfen auf Überlappung
+        if (currentInterval.start < previousInterval.end) {
+          const gap =
+            previousInterval.end - currentInterval.start + 24 * 60 * 60 * 1000; //Zeit der Überlappung berechnen + 1 Tag
+          currentInterval.start += gap;
+          currentInterval.end += gap;
+          intervals[i] = currentInterval; //Intervall in Zukunft schieben
+        } else if (currentInterval.start === previousInterval.end) {
+          currentInterval.start += 24 * 60 * 60 * 1000;
+          currentInterval.end += 24 * 60 * 60 * 1000;
+          intervals[i] = currentInterval; //Intervall in Zukunft schieben
+        } /*else if (currentInterval.start > previousInterval.end) {
+          //Alle Intervalle, welche nicht relevant sind entschärfen
+          for (let j = 0; j < i; j++) {
+            intervals[j].start = 0;
+            intervals[j].end = 0;
+          }
+        }*/
+      }
+      //Alle entschärften Intervalle entfernen
+      /*for (let k = intervals.length - 1; k >= 0; k--) {
+        if (intervals[k].start === 0 && intervals[k].end === 0) {
+          intervals.splice(k, 1);
+        }
+      }*/
+    }
+    return intervals;
+  }
+
+  function EndDate(intervals: Array<Intervall>): string | null {
+    if (intervals.length !== 0) {
+      const lastInterval = intervals[intervals.length - 1];
+      const today = new Date().getTime();
+      const remainingTime = lastInterval.end - today;
+      if (remainingTime < 1) {
+        return null;
+      } else {
+        const AboEnd = new Date(today + remainingTime);
+        const day = AboEnd.getDate().toString().padStart(2, "0"); // Tag mit führender Null, wenn nötig
+        const month = (AboEnd.getMonth() + 1).toString().padStart(2, "0"); // Monat mit führender Null, wenn nötig
+        const year = AboEnd.getFullYear();
+        const endDate = `${day}.${month}.${year}`;
+        return endDate;
+      }
+    } else {
+      return null;
+    }
+  }
+
+  /*       if (today < currentInterval.end) {
+          durationMs = currentInterval.end - today;
+        }
+
+    
+  } */
+  return (
+    <p>
+      {date !== null
+        ? "Dein Abonnement läuft ab am " + date
+        : "Kein Abonnement gefunden"}
+      .
+    </p>
+  );
 }
 
 export default AboDurationCalculator;
